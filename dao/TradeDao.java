db52405ed4d3f7e46859c2479e80b546a4d5065f
yutakashimazaki <y.s_official@YutakanoMacBook-Pro.local>
2019-11-20 00:59:30 +0900

    システムに売買取引機能を追加

---
+++ dao/TradeDao.java
@@ -0,0 +1,108 @@
+package dao;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+import java.math.BigDecimal;
+
+import model.data.Trade;
+import model.data.TradeType;
+/**
+  *取引ファイルに関する処理をするクラス。
+  */
+public class TradeDao {
+  /** 取引ファイル名 */
+  private final String filename = "csv/tradedata.csv";
+  /** 取引ファイルの存在自体を操作するためのPathインスタンス */
+  private Path path = Paths.get(this.filename);
+
+  /**
+    *取引ファイルが存在するか確認するメソッド
+    * @return 取引ファイルが存在すればtrue
+    */
+  private boolean isExsistTradeData() {
+    return Files.exists(path);
+  }
+  /**
+    *取引ファイルに新たな銘柄を追加するメソッド
+    * @param trade 銘柄コード・売買・取引価格・取引数量の情報
+    */
+  public void putTradeData(Trade trade) {
+    List<Trade> tradeList = null;
+    if(this.isExsistTradeData()) { //取引一覧ファイルが存在するときの処理
+      //取引の一覧を取得
+      tradeList = this.getTradeList();
+
+    } else { //取引一覧ファイルが存在しないときの処理
+      tradeList = new ArrayList<>();
+    }
+
+    //取引一覧の末尾に取引を追加
+    tradeList.add(trade);
+
+    //ファイルに書き込み
+    this.writeTradeData(tradeList);
+  }
+  /**
+    *取引ファイルから１行ずつリストに格納し返すメソッド
+    * @return 取引ファイルにある取引リスト
+    */
+  public List<Trade> getTradeList() {
+    List<Trade> tradeList = new ArrayList<>();
+
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new FileReader(filename));
+
+      String line = null;
+      while((line = br.readLine()) != null) {
+        String[] tradeData = line.split(",", -1);
+        Trade trade = new Trade(tradeData[0], TradeType.valueOf(tradeData[1]),
+                      new BigDecimal(tradeData[2]), new BigDecimal(tradeData[3]));
+        tradeList.add(trade);
+      }
+    } catch(IOException e) {
+      System.out.println(e);
+    } finally {
+      if(br != null){
+        try {
+          br.close();
+        } catch(Exception e2) {
+          System.out.println(e2);
+        }
+      }
+    }
+    return tradeList;
+  }
+  /**
+    *リストから１行ずつ取引ファイルに書き込むメソッド
+    * @param tradeList 取引のリスト
+    * @return 処理が成功するとtrue失敗するとfalse
+    */
+  public boolean writeTradeData(List<Trade> tradeList) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new FileWriter(filename,false));
+      //銘柄残高ファイルに書き込み
+      for(Trade trade : tradeList) {
+        pw.println(trade.toString());
+      }
+
+      return true;
+    } catch(IOException e) {
+      System.out.println(e);
+    } finally {
+      if(pw != null) {
+        try {
+          pw.close();
+        } catch(Exception e2) {
+          System.out.println(e2);
+        }
+      }
+    }
+    return false;
+  }
+}
--- model/logic/TradeBond.java
+++ model/logic/TradeBond.java
@@ -5,8 +5,11 @@ import java.math.BigDecimal;
 
 import dao.BondDao;
 import dao.BalanceDao;
+import dao.TradeDao;
 import model.data.Bond;
 import model.data.Balance;
+import model.data.Trade;
+import model.data.TradeType;
 
 /**
   *売買取引データの入力処理。
@@ -17,9 +20,11 @@ public class TradeBond {
   private BondDao bondDao = new BondDao();
   /** 残高データのDAO　*/
   private BalanceDao balanceDao = new BalanceDao();
+  /** 取引データのDAO　*/
+  private TradeDao tradeDao = new TradeDao();
 
   /**
-    *在庫データを入力する処理をするメソッド
+    *取引データを入力する処理をするメソッド
     */
   public void execute() {
     while(true) {
@@ -42,12 +47,41 @@ public class TradeBond {
           continue;
         }
 
-        //保有数量と新たな簿価の入力
-        System.out.print("保有数量>");
-        BigDecimal amountToBeAdded = new BigDecimal(br.readLine());
-        System.out.print("簿価>");
-        BigDecimal bookValueToBeAdded = new BigDecimal(br.readLine());
+        //売買入力
+        System.out.print("0:売 1:買>");
+        int typeId = Integer.parseInt(br.readLine());
+        //入力が「売買」以外のときはエラーを表示
+        if(typeId != 0 && typeId != 1) {
+          System.out.println("0(売)か1(買)で指定してください。\n");
+          continue;
+        }
+        //売りの場合でその銘柄を持っていない場合はエラーを表示
+        if(typeId == 0 && !this.balanceDao.isExistBond(code)) {
+          System.out.println("この銘柄を持っていないため、売ることはできません。");
+          continue;
+        }
 
+        //価格・数量の入力
+        TradeType tradeType = TradeType.valueOf(typeId);
+        System.out.print("価格>");
+        BigDecimal tradePrice = new BigDecimal(br.readLine());
+        System.out.print("数量>");
+        BigDecimal tradeAmount = new BigDecimal(br.readLine());
+
+        //取引データの作成（価格・数量がマイナスの時は例外を返す）
+        Trade trade = new Trade(code, tradeType, tradePrice, tradeAmount);
+
+        //取引金額
+        BigDecimal price = tradePrice.multiply(tradeAmount);
+
+        //計算のために取引が「売り」の場合は数量をマイナスにする
+        if(tradeType == TradeType.SELL) {
+          tradeAmount = BigDecimal.valueOf(0).subtract(tradeAmount);
+        }
+
+        //銘柄残高ファイルへの書き込み・実現損益の計算
+        //実現損益
+        BigDecimal realizedProfit = null;
         //指定のコードの銘柄を保有しているか確認
         if(this.balanceDao.isExistBond(code)) { //保有しているときの処理
           //銘柄の保有数量と簿価を取得
@@ -56,7 +90,7 @@ public class TradeBond {
           BigDecimal oldBookValue = balance.getBookValue();
 
           //保有数量の更新
-          BigDecimal newAmount = oldAmount.add(amountToBeAdded);
+          BigDecimal newAmount = oldAmount.add(tradeAmount);
 
           //保有数量が0未満ならエラーを表示
           if(newAmount.compareTo(BigDecimal.valueOf(0)) == -1) {
@@ -69,7 +103,7 @@ public class TradeBond {
           if(newAmount.equals(BigDecimal.valueOf(0))) {
             newBookValue = BigDecimal.valueOf(0);
           } else {
-            newBookValue = (oldAmount.multiply(oldBookValue).add(amountToBeAdded.multiply(bookValueToBeAdded)))
+            newBookValue = (oldAmount.multiply(oldBookValue).add(tradeAmount.multiply(tradePrice)))
                                     .divide(newAmount,3,BigDecimal.ROUND_DOWN);
           }
 
@@ -78,20 +112,39 @@ public class TradeBond {
           balance.setBookValue(newBookValue);
           this.balanceDao.updateBalanceData(balance);
 
+          //実現損益の計算
+          realizedProfit = tradePrice.subtract(oldBookValue).multiply(BigDecimal.valueOf(0).subtract(tradeAmount));
+
         } else { //保有していないときの処理
           //保有数量が0未満ならエラーを表示
-          if(amountToBeAdded.compareTo(BigDecimal.valueOf(0)) == -1) {
-            System.out.println("保有数量がマイナスです。");
-            continue;
-          }
+          // if(tradeAmount.compareTo(BigDecimal.valueOf(0)) == -1) {
+          //   System.out.println("保有数量がマイナスです。");
+          //   continue;
+          // }
 
-          Balance balance = new Balance(code, amountToBeAdded, bookValueToBeAdded);
+          Balance balance = new Balance(code, tradeAmount, tradePrice);
           this.balanceDao.putBalanceData(balance);
         }
+
+        //取引金額と実現損益の表示
+        System.out.print("取引金額 : " + price);
+        //取引が「売り」の場合は実現損益を表示する
+        if(tradeType == TradeType.SELL) {
+          System.out.println(" 実現損益 : " + realizedProfit + "\n");
+        } else {
+          System.out.println("\n");
+        }
+
+        //取引リストの更新
+        tradeDao.putTradeData(trade);
+
+
       } catch(IOException e) {
         System.out.println(e);
-      } catch(NumberFormatException ee) {
+      } catch(NumberFormatException e2) {
         System.out.println("数字を入力してください。");
+      } catch(IllegalArgumentException e3) {
+        System.out.println(e3);
       }
     }
 
